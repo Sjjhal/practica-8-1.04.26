caaa3b2ff826773603170b068849d7c9669b6cf8
Sjjhal <lolk57462@gmail.com>
2026-04-02 19:57:53 +0300

    Delete NevirnaPochtkovaRozmirnistVynatok (1).java

--- NevirnaPochtkovaRozmirnistVynatok (1).java
+++
@@ -1,5 +0,0 @@
-public class NevirnaPochtkovaRozmirnistVynatok extends Exception {
-    public NevirnaPochtkovaRozmirnistVynatok(int mistkist) {
-        super("Неприпустима початкова місткість: " + mistkist + ". Значення має бути більше 0");
-    }
-}
